7dc749aff48c3eb8fa70b3a0aad7d26a6882a96e
Mark Kremer <mark@without-brains.net>
2012-02-28 14:11:39 +0100

    Instead of overriding initialize use the option macros, this allows you to use a lambda to override the defaults per request

--- lib/omniauth/strategies/google.rb
+++ lib/omniauth/strategies/google.rb
@@ -8,29 +8,23 @@ module OmniAuth
     # Usage:
     #    use OmniAuth::Strategies::Google, 'consumerkey', 'consumersecret'
     class Google < OmniAuth::Strategies::OAuth
-      def initialize(app, consumer_key=nil, consumer_secret=nil, options={}, &block)
-        client_options = {
-          :access_token_path => '/accounts/OAuthGetAccessToken',
-          :authorize_path => '/accounts/OAuthAuthorizeToken',
-          :request_token_path => '/accounts/OAuthGetRequestToken',
-          :site => 'https://www.google.com',
-        }
-        google_contacts_auth = 'www.google.com/m8/feeds'
-        options[:scope] ||= "https://#{google_contacts_auth}"
-        options[:scope] << " https://#{google_contacts_auth}" unless options[:scope] =~ %r[http[s]?:\/\/#{google_contacts_auth}]
-        options[:client_options] = client_options
+      option :client_options, {
+        :access_token_path => '/accounts/OAuthGetAccessToken',
+        :authorize_path => '/accounts/OAuthAuthorizeToken',
+        :request_token_path => '/accounts/OAuthGetRequestToken',
+        :site => 'https://www.google.com'
+      }
 
-        super(app, consumer_key, consumer_secret, options, &block)
-      end
+      option :scope, "https://www.google.com/m8/feeds"
 
       uid do
         user_info['uid']
       end
-      
+
       info do
         user_info
       end
-      
+
       extra do
         { 'user_hash' => user_hash }
       end
@@ -63,20 +57,20 @@ module OmniAuth
       def request_phase
         request_options = {:scope => options[:scope]}
         request_options.merge!(options[:authorize_params])
-      
+
         request_token = consumer.get_request_token({:oauth_callback => callback_url}, request_options)
         session['oauth'] ||= {}
         session['oauth'][name.to_s] = {'callback_confirmed' => request_token.callback_confirmed?, 'request_token' => request_token.token, 'request_secret' => request_token.secret}
         r = Rack::Response.new
-      
+
         if request_token.callback_confirmed?
           r.redirect(request_token.authorize_url)
         else
           r.redirect(request_token.authorize_url(:oauth_callback => callback_url))
         end
-      
+
         r.finish
-      
+
         rescue ::Timeout::Error => e
           fail!(:timeout, e)
         rescue ::Net::HTTPFatalError, ::OpenSSL::SSL::SSLError => e
